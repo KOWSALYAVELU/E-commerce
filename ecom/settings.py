fad34234fe5d1ccbabf468e229aa57f653827244
KOWSALYAVELU <147810565+KOWSALYAVELU@users.noreply.github.com>
2026-09-23 17:27:27 +0530

    Fix static and media file serving for Vercel

--- ecom/settings.py
+++ ecom/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'jazzmin',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,13 +115,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/image/'
 MEDIA_ROOT = BASE_DIR / 'static'
-
-STATICFILES_DIRS=[
-    BASE_DIR/ 'static'
-]
 
  
 
